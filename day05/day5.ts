26de31a86db2fff422f42749b6f5a42e849e8320
Patrick McCarthy <p.t.mccarthy@gmail.com>
2017-12-20 15:51:00 -0600

    Solve day 5 part 2

--- day05/day5.ts
+++ day05/day5.ts
@@ -5,7 +5,7 @@ import * as _ from 'lodash';
 const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8').trim().split('\n');
 const offsets = input.map(i => parseInt(i, 10));
 
-const runInstructions = (offsets: number[]) => {
+const runInstructionsPart1 = (offsets: number[]) => {
   const length = offsets.length;
   let cursor = 0;
   let jumps = 0;
@@ -20,5 +20,29 @@ const runInstructions = (offsets: number[]) => {
   return jumps;
 };
 
-const part1 = runInstructions(offsets);
+const runInstructionsPart2 = (offsets: number[]) => {
+  const length = offsets.length;
+  let cursor = 0;
+  let jumps = 0;
+
+  while (cursor < length) {
+    const jump = offsets[cursor];
+    
+    if (jump < 3) {
+      offsets[cursor] += 1;
+    } else {
+      offsets[cursor] -= 1;
+    }
+
+    cursor += jump;
+    jumps += 1;
+  }
+
+  return jumps;
+};
+
+const part1 = runInstructionsPart1(_.cloneDeep(offsets));
 console.log(`Part 1: ${part1}`);
+
+const part2 = runInstructionsPart2(_.cloneDeep(offsets));
+console.log(`Part 2: ${part2}`);
